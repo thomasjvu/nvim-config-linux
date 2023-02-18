55ac6695a822d7c3f8a8d3ada5b1a79b73cc2d60
Thomas J. Vu <thomasjvu@gmail.com>
2023-02-18 03:17:27 -0500

    Update

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,6 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'spacegray',
     },
     sections = {
         lualine_a = {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     path = "/home/aos/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/home/aos/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/home/aos/.local/share/nvim/site/pack/packer/start/vim-fugitive",
